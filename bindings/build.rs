5775192c4a41930ba7dde62bf1442fd6df9ea0e3
Kingloo <k1ngloo@hotmail.com>
2021-09-16 01:46:57 +0200

    Remove unnecessary binding

--- bindings/build.rs
+++ bindings/build.rs
@@ -2,6 +2,6 @@ fn main() {
 	windows::build! {
 		Windows::Win32::Foundation::{PWSTR},
 		Windows::Win32::UI::Shell::{IDesktopWallpaper, DesktopWallpaper},
-		Windows::Win32::System::Com::{CoInitializeEx, CoCreateInstance, CLSCTX}
+		Windows::Win32::System::Com::{CoInitializeEx, CoCreateInstance}
 	};
 }
